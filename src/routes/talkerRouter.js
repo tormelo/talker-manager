5c698340cad17190523c6ab0d6ebb79da54614df
tormelo <23294699+tormelo@users.noreply.github.com>
2022-11-04 15:22:31 -0300

    implementa endpoint GET /talker

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { readTalkersData } = require('../utils/fsUtils');
 
 const router = express.Router();
 
-router.get('/talker', (req, res) => {
-  res.status(200).send();
+router.get('/talker', async (req, res) => {
+  const talkers = await readTalkersData();
+  res.status(200).json(talkers);
 });
 
 module.exports = router;
