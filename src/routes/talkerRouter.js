7c9943d3c22d4e64da5d1d727fa049bdc746502f
tormelo <23294699+tormelo@users.noreply.github.com>
2022-11-04 15:41:26 -0300

    implementa endpoint GET /talker/:id

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -8,4 +8,16 @@ router.get('/talker', async (req, res) => {
   res.status(200).json(talkers);
 });
 
+router.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalkersData();
+  const talker = talkers.find((t) => Number(id) === t.id);
+  
+  if (talker) return res.status(200).json(talker);
+  
+  res.status(404).json({
+    message: 'Pessoa palestrante não encontrada',
+  });
+});
+
 module.exports = router;
