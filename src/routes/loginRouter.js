5d37ff9df6257cb44fa7ab65a5fdb4d3e993a797
tormelo <23294699+tormelo@users.noreply.github.com>
2022-11-04 16:15:03 -0300

    implementa endpoint POST /login

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cryptoJS = require('crypto-js');
 
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
-  res.status(200).send();
+  const { email, password } = req.body;
+  const token = cryptoJS.lib.WordArray.random(8).toString();
+  console.log(email, password);
+  console.log(token);
+  res.status(200).json({ token });
 });
 
 module.exports = router;
